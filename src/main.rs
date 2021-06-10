c04dc2de88dfbe4e8d2d3216c289163a5808ab0b
Charlie Thomson <charliecthomson@gmail.com>
2021-06-10 17:06:16 -0400

    Clean main up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
 pub mod parser;
 use std::{
-    fs,
-    io::{self, Read, Write},
+    fs::{self, File},
+    io::{self, stdin, stdout, Read, Write},
     path::PathBuf,
 };
 
-fn get_examples() -> io::Result<Vec<PathBuf>> {
-    let available_examples = fs::read_dir("./examples")?
-        .map(|entry| entry.unwrap().path())
-        .collect::<Vec<PathBuf>>();
-
-    Ok(available_examples)
-}
-
 fn get_index(available_examples: &Vec<PathBuf>) -> io::Result<usize> {
     let mut index = usize::MAX;
+    let mut input = String::new();
 
     while index >= available_examples.len() {
         print!(
             "Which would you like to run (a number 0 - {}): ",
             available_examples.len() - 1
         );
-        io::stdout().flush()?;
+        stdout().flush()?;
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-        index = usize::from_str_radix(&input.trim(), 10).unwrap();
+        stdin().read_line(&mut input)?;
 
-        if index >= available_examples.len() {
-            println!("{} is out of range, try again.", index);
+        match usize::from_str_radix(&input.trim(), 10) {
+            Ok(idx) if idx < available_examples.len() => index = idx,
+            Ok(idx) => {
+                println!("{} is out of range, try again.", idx);
+            }
+            _ => {
+                println!("{:?} is not a valid number, try again.", input.trim());
+            }
         }
+
+        input.clear();
     }
 
     Ok(index)
 }
 
 fn main() -> io::Result<()> {
-    let available_examples = get_examples()?;
+    let available_examples = fs::read_dir("./examples")?
+        .map(|entry| entry.unwrap().path())
+        .collect::<Vec<PathBuf>>();
 
     println!("Available Examples: ");
 
@@ -45,16 +46,19 @@ fn main() -> io::Result<()> {
         .enumerate()
         .for_each(|(idx, path)| println!("\t{}: {:?}", idx, path.file_name().unwrap()));
 
-    let index = get_index(&available_examples)?;
+    let user_index = get_index(&available_examples)?;
 
-    let example_path = available_examples.get(index).unwrap();
-    println!("You selected:\n\t{}: {:?}", index, example_path);
+    let selected_example_path = available_examples.get(user_index).unwrap();
+    println!(
+        "You selected:\n\t{}: {:?}",
+        user_index, selected_example_path
+    );
 
     let mut file_contents = String::new();
 
-    let nbytes = fs::File::open(example_path)?.read_to_string(&mut file_contents)?;
-    println!("Read {} bytes from {:?}", nbytes, example_path);
+    let nbytes = File::open(selected_example_path)?.read_to_string(&mut file_contents)?;
 
+    println!("Read {} bytes from {:?}", nbytes, selected_example_path);
     println!("Parsing...");
     println!("Result: {:#?}", parser::Parser::new().parse(&file_contents));
     Ok(())
